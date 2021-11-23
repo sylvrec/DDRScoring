d241a7bed2c39cd2dddcc879580c9b6e63833875
Sylvrec <sylvrec@gmail.com>
2021-11-23 20:09:24 +0100

    Resolve conflict.

--- DDRScoring/Startup.cs
+++ DDRScoring/Startup.cs
@@ -1,33 +1,83 @@
+using DDRScoring.Data;
+using DDRScoring.Data.Entities;
+using DDRScoring.Data.Repository;
+using DDRScoring.Data.Repository.impl;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DDRScoring
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        private readonly IConfiguration _config;
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<StoreUser, IdentityRole>(cfg =>
+            {
+                cfg.User.RequireUniqueEmail = true;
+                cfg.ClaimsIdentity.UserIdClaimType = "UserID";
+            }).AddEntityFrameworkStores<DDRScoringContext>();
+            services.AddAuthentication(cfg =>
+            {
+                cfg.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                cfg.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                cfg.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(cfg =>
+                {
+                    cfg.SaveToken = true;
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = _config["Tokens:Issuer"],
+                        ValidAudience = _config["Tokens:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]))
+                    };
+                });
+            services.AddDbContext<DDRScoringContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config["ConnectionStrings:DDRScoringContextDb"]);
+            });
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddTransient<DDRScoringSeeder>();
+            AddServiceRepository(services);
             services.AddRazorPages();
             services.AddControllers().AddXmlSerializerFormatters();
+        }
+
+        private void AddServiceRepository(IServiceCollection services)
+        {
+            services.AddTransient<IPlayerRepository, PlayerRepository>();
+            services.AddTransient<IStepsRepository, StepsRepository>();
+            services.AddTransient<ISongRepository, SongRepository>();
+            services.AddTransient<IHighScoreRepository, HighScoreRepository>();
+            services.AddTransient<IHighScoreListRepository, HighScoreListRepository>();
+            services.AddTransient<IRadarValuesRepository, RadarValuesRepository>();
+            services.AddTransient<ITapNoteScoresRepository, TapNoteScoresRepository>();
+            services.AddTransient<ICaloriesBurnedRepository, CaloriesBurnedRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +98,8 @@ namespace DDRScoring
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
